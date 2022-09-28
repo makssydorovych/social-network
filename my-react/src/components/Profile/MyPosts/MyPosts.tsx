4104bc8c5b99fe0b3ed7866ef42db3bebafc0081
Maksym <maksymsydorovych@gmail.com>
2022-09-28 17:32:10 +0200

    ts

--- my-react/src/components/Profile/MyPosts/MyPosts.tsx
+++ my-react/src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,23 +1,24 @@
 import s from "./MyPosts.module.css";
 import Posts from "./Post/Posts";
 import React from 'react';
-import {PostType, ProfilePageType} from "../../../redux/state";
+import {PostType} from "../../../redux/state";
 type PropsType = {
 	posts: Array<PostType>
 	addPost: (postMessage: string) => void
 	updateNewPostText: (newText: string) => void
 }
+
 const MyPosts = (props : PropsType) => {
 	let postsElemetns = 
 	props.posts.map ( p => <Posts  message={p.message} likesCount={p.likesCount} id={p.id}/>);
 	let newPostElement = React.createRef();
 
-	let addPost = () => {
+	let addPost = (postMessage: string) => {
 		let text  = newPostElement.current.value;
 		props.addPost(text);
 		props.updateNewPostText("");
 	};
-	let onPostChange = () => {
+	let onPostChange = (postMessage: string) => {
 		let text = newPostElement.current.value;
 		props.updateNewPostText(text);
 		props.updateNewPostText("");
