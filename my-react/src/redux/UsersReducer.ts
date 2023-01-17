5254d7160850f2d5d7b6d2b105f316e9a1ca2320
Maksym <maksymsydorovych@gmail.com>
2023-01-17 11:45:11 +0100

    Finish typo users, mypost, dialog 2.0

--- my-react/src/redux/UsersReducer.ts
+++ my-react/src/redux/UsersReducer.ts
@@ -1,5 +1,4 @@
-
-type ActionsTypes =  FollowActionType | UnfollowActionType |
+type ActionsTypes = FollowActionType | UnfollowActionType |
     SetUsersActionType;
 export type UserType = {
     id: number
@@ -7,45 +6,71 @@ export type UserType = {
     followed: boolean
     fullName: string
     status: string
-    location : {city: string, country: string}
+    location: { city: string, country: string }
 }
 const initialState = {
     users: [
-        {id: 1, photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",followed: true, fullName: "Hello", status: "status", location:{city: "city", country:"country"}},
-        {id: 2, photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",followed: true, fullName: "Hello", status: "status", location:{city: "city", country:"country"}},
-        {id: 3, photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",followed: false, fullName: "Hello", status: "status", location:{city: "city", country:"country"}},
-        {id: 4, photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",followed: false, fullName: "Hello", status: "status", location:{city: "city", country:"country"}},
+        {
+            id: 1,
+            photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",
+            followed: true,
+            fullName: "Hello",
+            status: "status",
+            location: {city: "city", country: "country"}
+        },
+        {
+            id: 2,
+            photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",
+            followed: true,
+            fullName: "Hello",
+            status: "status",
+            location: {city: "city", country: "country"}
+        },
+        {
+            id: 3,
+            photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",
+            followed: false,
+            fullName: "Hello",
+            status: "status",
+            location: {city: "city", country: "country"}
+        },
+        {
+            id: 4,
+            photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png",
+            followed: false,
+            fullName: "Hello",
+            status: "status",
+            location: {city: "city", country: "country"}
+        },
 
     ] as Array<UserType>,
 
 };
 export type UsersInitialStateType = typeof initialState
-export const usersReducer = (state:UsersInitialStateType = initialState, action: ActionsTypes):UsersInitialStateType => {
+export const usersReducer = (state: UsersInitialStateType = initialState, action: ActionsTypes): UsersInitialStateType => {
 
     switch (action.type) {
         case 'FOLLOW':
-            return{
+            return {
                 ...state,
-                users: state.users.map(u =>{
-                    if(u.id === action.userId) {
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
                         return {...u, followed: true}
                     }
-                    return  u;
+                    return u;
                 })
             }
 
         case 'UNFOLLOW' :
-            return{
+            return {
                 ...state,
-                users: state.users.map(u =>{
-                    if(u.id === action.userId) {
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
                         return {...u, followed: false}
                     }
-                    return  u;
+                    return u;
                 })
             }
-
-            return state
         case 'SET-USERS' :
             return {...state, users: [...state.users, ...action.users]}
         default:
@@ -53,18 +78,18 @@ export const usersReducer = (state:UsersInitialStateType = initialState, action:
 
     }
 }
-export const followAC = (userId:number) => {
-   return {
+export const followAC = (userId: number) => {
+    return {
         type: 'FOLLOW', userId
     } as const
 }
-export const unfollowAC = (userId:number) => {
+export const unfollowAC = (userId: number) => {
     return {
         type: 'UNFOLLOW', userId
     } as const
 }
-export const setUsersAC = (users: any) =>{
-   return {
+export const setUsersAC = (users: any) => {
+    return {
         type: 'SET-USERS', users
     } as const
 }
