647409b15e3473f1ecc4fdecae62eb9837489890
Maksym <maksymsydorovych@gmail.com>
2023-01-30 22:59:09 +0100

    validator Fields

--- my-react/src/utils/validator.ts
+++ my-react/src/utils/validator.ts
@@ -1,8 +1,13 @@
-export const required = value => {
+
+export type FieldValidatorType =(value: string)=>string|undefined
+
+
+
+export const required: FieldValidatorType = (value)=> {
     if(value) return undefined
     return "Field is required"
 }
-export const maxLengthCreator = (maxLength)=>(value)=>{
+export const maxLengthCreator = (maxLength:number): FieldValidatorType=>(value)=>{
     if(value.length > maxLength)return `Max length is ${maxLength} symbols`
     return undefined
 }
