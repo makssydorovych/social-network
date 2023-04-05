6ec3d99a7eeaa954d48b291ca12344aacc4c5587
Maksym <maksymsydorovych@gmail.com>
2023-04-05 14:53:25 +0200

    delete my-react folder

--- my-react/src/redux/sidebar-reducer.ts
+++
@@ -1,7 +0,0 @@
-let initialState = {
-}
-type InitialStatetype = typeof initialState
-
-export const sidebarReducer = (state = initialState, action: any): InitialStatetype =>{
-    return state
-}
--- my-react/src/utils/object-helper.ts
+++
@@ -1,8 +0,0 @@
-export const updateObjectInArray = (items: any, itemId: number, objPropName: any, newObjProps: any) => {
-    return items.map((usr: any) => {
-        if (usr[objPropName] === itemId) {
-            return {...usr, ...newObjProps}
-        }
-        return usr;
-    })
-}
