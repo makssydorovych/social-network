588a4fe7c1008f7b65a02de6691b40cea701eec0
Maksym <maksymsydorovych@gmail.com>
2023-04-05 19:09:22 +0200

    preloader, small preloader

---
+++ src/components/00-Common/PreloaderSmall/PreloaderSmall.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import preload from '../../../assets/images/ajax-loader.gif';
+
+export const PreloaderSmall = () => {
+    return (
+        <div>
+            <img src={preload} alt="smallLoader" />
+        </div>
+    );
+};
